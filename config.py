89cd5605f8140ed30796e936a5f85243c7276d08
vsynovyd <vsinevid660@gmail.com>
2024-07-20 14:51:55 -0700

    Update config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+CLIENT_ID = 'e49f04b4-507b-4e1d-801b-5433c047f53e'
+CLIENT_SECRET = 'f23d5e30-8cf8-40e6-938a-df2898ed2927'
+TENANT_ID = '6f9b6b26-53a8-4387-b4de-8f8b16e205ee'
+REDIRECT_URI = 'http://localhost'
